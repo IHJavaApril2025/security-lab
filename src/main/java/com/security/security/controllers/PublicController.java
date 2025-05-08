a85d0e4aab4412199f4083677fcdc967fa6f760f
Josemargal <126793937+Josemargal@users.noreply.github.com>
2025-05-08 10:42:22 +0200

    solucion lab

---
+++ src/main/java/com/security/security/controllers/PublicController.java
@@ -0,0 +1,19 @@
+package com.security.security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/public")
+public class PublicController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello! This is a public endpoint.";
+    }
+}
+
+
+
+
---
+++ src/main/java/com/security/security/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.security.security.repositories;
+
+import com.security.security.models.ERole;
+import com.security.security.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
